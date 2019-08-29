cf3319ec958245a81c5b29e7c6dfb18d73f8e0be
Radek <radoslaw.dabrowski@boldare.com>
2019-08-29 16:42:58 +0200

    netty

--- src/main/java/com/boldare/websocketshowcase/websocket/ReactiveWebsocketConfiguration.java
+++ src/main/java/com/boldare/websocketshowcase/websocket/ReactiveWebsocketConfiguration.java
@@ -1,4 +1,62 @@
 package com.boldare.websocketshowcase.websocket;
 
+import com.boldare.websocketshowcase.integration.MessageGateway;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Collections;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class ReactiveWebsocketConfiguration {
+
+    private final MessageGateway gateway;
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+        SimpleUrlHandlerMapping simpleUrlHandlerMapping = new SimpleUrlHandlerMapping();
+        simpleUrlHandlerMapping.setUrlMap(
+                Collections.singletonMap("/ocpp/{chargerName}", webSocketHandler()));
+        simpleUrlHandlerMapping.setCorsConfigurations(
+                Collections.singletonMap("*", new CorsConfiguration().applyPermitDefaultValues()));
+        simpleUrlHandlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return simpleUrlHandlerMapping;
+    }
+
+    @Bean
+    WebSocketHandlerAdapter webSocketHandlerAdapter() {
+        return new WebSocketHandlerAdapter();
+    }
+
+    private String ackId = null;
+
+    private WebSocketHandler webSocketHandler() {
+        return session ->
+                session.send(
+                        //send ack
+                        Flux.interval(Duration.ofSeconds(1))
+                                .map(Object::toString)
+                                .map(payload -> session.textMessage("[3," + ackId + ", {}]"))
+                ).and(session.receive()
+                        .map(WebSocketMessage::getPayloadAsText)
+                        .doOnNext(payload -> {
+                            ackId = payload.split(",")[1];
+                            gateway.processMessage(payload);
+                        })
+                );
+    }
+
 }
--- src/main/java/com/boldare/websocketshowcase/websocket/client/WebsocketClient.java
+++ src/main/java/com/boldare/websocketshowcase/websocket/client/WebsocketClient.java
@@ -1,4 +1,28 @@
 package com.boldare.websocketshowcase.websocket.client;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+import org.springframework.web.reactive.socket.client.WebSocketClient;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.time.Duration;
+
+@Slf4j
 public class WebsocketClient {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebSocketClient client = new ReactorNettyWebSocketClient();
+        client.execute(
+                URI.create("ws://localhost:8099/ocpp/1231231"),
+                session -> session.send(
+                        Mono.just(session.textMessage("event-spring-reactive-client-websocket")))
+                        .thenMany(session.receive()
+                                .map(WebSocketMessage::getPayloadAsText)
+                                .log())
+                        .then())
+                .block(Duration.ofSeconds(10));
+    }
+
 }
